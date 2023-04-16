efad3899be873e4ef297ae3e49ec57df686df631
Kyle Luo <kluo15@ucsc.edu>
2023-04-15 20:32:24 -0700

    Removing database

--- MindMatch/MindMatch/settings.py
+++ MindMatch/MindMatch/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '*.vercel.app',
+    'MindMatch.vercel.app',
+    '.vercel.app',
     '127.0.0.1'
     ]
 
@@ -79,10 +81,10 @@ WSGI_APPLICATION = 'MindMatch.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
