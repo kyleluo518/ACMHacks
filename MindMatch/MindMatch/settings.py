c1605af3986d66c7fa186caa1263375cb237ec6b
Kyle Luo <kluo15@ucsc.edu>
2023-04-15 20:06:07 -0700

    Removed key

--- MindMatch/MindMatch/settings.py
+++ MindMatch/MindMatch/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@jsg&xlahkt56k6psq&ngydx*iwiug9fc@7mzs$^b+oa9b50gc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
